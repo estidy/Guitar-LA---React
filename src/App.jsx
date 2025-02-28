43870e3a836b4150a1f810b1b8d66bb812affb3a
Juana Diaz <estidy@gmail.com>
2025-02-28 01:12:36 -0300

    agregando props a mi proyecto. para mostrar todas las guitarras de mi base de datos

--- src/App.jsx
+++ src/App.jsx
@@ -15,8 +15,11 @@ const [data, setData] = useState(db)
         <h2 className="text-center">Nuestra Colección</h2>
 
         <div className="row mt-5">
-            {data.map(()=>(
-            <Guitar />
+            {data.map((guitar)=>(
+            <Guitar 
+                key={guitar.id}
+                guitar={guitar} 
+            />
             ))}
             
         </div>

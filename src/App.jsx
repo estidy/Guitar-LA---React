63d25ec9dc8dd4d671d10996a19c6be955a6ddfd
Juana Diaz <estidy@gmail.com>
2025-02-28 21:01:44 -0300

    agregando mi state para el carrito. La funcion para agregar guitarras junto con la cantidad a comprar

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,21 @@ import { db } from './Data/db'
 function App() {
   
 const [data, setData] = useState(db)
+const [cart, setCart] = useState([])
+
+function addToCart(item){
+    const itemExist = cart.findIndex(guitar => guitar.id === item.id)
+    if (itemExist >= 0){
+         const updatedCart = [...cart]
+         updatedCart[itemExist].quantity++
+         setCart(updatedCart)
+         console.log("Este producto ya existe")
+    }else{
+         console.log("Este producto no existe")
+         item.quantity = 1
+         setCart([...cart, item])
+    }
+}
 
   return (
     <>
@@ -17,8 +32,9 @@ const [data, setData] = useState(db)
         <div className="row mt-5">
             {data.map((guitar)=>(
             <Guitar 
-                key={guitar.id}
+                key={guitar.id}                
                 guitar={guitar} 
+                addToCart={addToCart}
             />
             ))}
             
